8431e2bddc4090a24e88c2b8e593fedf97205154
Shaik-Banglr <153170456+Shaik-Banglr@users.noreply.github.com>
2024-09-05 00:22:30 +0530

    Add files via upload

---
+++ ColorFormatter.cpp
@@ -0,0 +1,24 @@
+#include "ColorFormatter.h"
+#include <map>
+
+using namespace TelCoColorCoder;
+
+std::string ColorFormatter::printcolorcodes()
+{
+	std::string colorcodes;
+	std::map<int, std::string> colorpairs;
+
+ for (auto pairNumber = 1; pairNumber <= 25; pairNumber++)
+ {
+	 auto color = ColorPair::GetColorFromPairNumber(pairNumber);
+	 colorpairs[pairNumber] = color.ToString();
+ }
+ for (auto itr :colorpairs)
+ {
+	 colorcodes.append(std::to_string(itr.first));
+	 colorcodes.append("--");
+	 colorcodes.append(itr.second);
+	 colorcodes.append("\n");
+ }
+ return colorcodes;
+}
