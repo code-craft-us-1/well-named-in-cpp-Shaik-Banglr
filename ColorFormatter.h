d7526707cf3967fb9b2ceb3aa2d773dccbae6df1
Shaik-Banglr <153170456+Shaik-Banglr@users.noreply.github.com>
2024-09-05 10:59:08 +0530

    Add files via upload

---
+++ ColorFormatter.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<string>
+class ColorFormatter
+{
+public:
+	static std::string printcolorcodes();
+};
---
+++ ColorPairTest.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <assert.h>
+#include "ColorPair.h"
+using namespace TelCoColorCoder;
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::ColorPair::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::ColorPair::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
+
